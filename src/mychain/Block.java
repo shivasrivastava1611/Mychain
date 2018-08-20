31e53ecbe1e2ec9ce5d54095320cc2de73eac593
Srivastava, Shiva <shivasrivastava1611@gmail.com>
2018-08-20 10:15:33 +0530

    Changes.

--- src/mychain/Block.java
+++ src/mychain/Block.java
@@ -7,19 +7,19 @@ public class Block {
 	public String hash;
 	public String previousHash; 
 	private String data; 
-	private long timeStamp; //as number of milliseconds since 1/1/1970.
+	private long timeStamp;
 	private int nonce;
 	
-	//Block Constructor.  
+	
 	public Block(String data,String previousHash ) {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
 		
-		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+		this.hash = calculateHash();
 	}
 	
-	//Calculate new hash based on blocks contents
+	
 	public String calculateHash() {
 		String calculatedhash = StringUtil.getSha256( 
 				previousHash +
